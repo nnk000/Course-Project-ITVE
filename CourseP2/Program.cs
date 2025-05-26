4dff3395f37ce8b76ca5962d0ca131faa6d365f1
FumoFumo022 <73023316+nnk000@users.noreply.github.com>
2025-05-26 06:08:07 +0300

    Add files via upload

---
+++ CourseP2/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // как я понял, можно либо по F/s, либо через Eε решать
+            Console.WriteLine("σ=F/s=Eε    механич. напряжение, Па");
+            Console.WriteLine("================");
+            Console.Write("Решение будет по той формуле, значения для которой ~= 0.\nЕсли подходят обе, то берётся случайная\n");
+            Console.WriteLine("================");
+            //
+            // Значение для проверки валидности, если F и S валидны
+            int choice = 0;
+            //
+            // Тут очень крутая проверка чтобы эксепшены не кидало, на Lua кстати легче, там есть проверка на валидность
+            // через IsValid() и просто оператором or
+            // local numF = GetNumber() or 0
+            //
+            Console.WriteLine("Введите F: ");
+            double.TryParse(Console.ReadLine(), out double numF);
+            //
+            Console.WriteLine("Введите s:");
+            double.TryParse(Console.ReadLine(), out double numS);
+            //
+            Console.WriteLine("Введите E:");
+            double.TryParse(Console.ReadLine(), out double numE);
+            //
+            Console.WriteLine("Введите ε:");
+            double.TryParse(Console.ReadLine(), out double numExt);
+            //
+            if (numF != 0 && numS != 0)
+            {
+                choice = 1;
+            }
+            if (numE != 0 && numExt != 0)
+            {
+                choice = 2;
+            }
+            if (numF != 0 && numS != 0 && numE != 0 && numExt != 0)
+            {
+                Random random = new Random();
+                choice = random.Next(1, 2);
+            }
+            //
+            switch(choice)
+            {
+                case 0:
+                    Console.WriteLine("Невозможно выполнить операцию.");
+                    break;
+                case 1:
+                    Console.WriteLine("Механическое напряжение = " + Math.Round(numF/numS, 2) + " Па");
+                    break;
+                case 2:
+                    Console.WriteLine("Механическое напряжение = " + Math.Round(numE*numExt, 2) + " Па");
+                    break;
+            }
+        }
+    }
+}
---
+++ courseP1/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вот что в шарпе мне не зашло так это строгое разграничение по строкам (в прочем, как и везде).
+            // Однако в Lua можно сделать вот так:
+            // 
+            // local text = "Хоффман развил свою предыдущую работу по моделированию TFT [27]
+            // исследованиями на DC (постоянном токе) до модели тонкопленочного транзистора с длинным каналом,
+            // что очень важно при расчете зависимости подвижности от напряжения на затворе [46]."
+            //
+            // тейблы тоже можно так делать
+            //
+            // JMod.EZ_GRADE_UPGRADE_COSTS = {.5, 1, 1.5, 2}
+            //
+            // JMod.PrimitiveResourceTypes = {
+            //      ["wood"] = { JMod.EZ_RESOURCE_TYPES.WOOD },
+            //      ["metal"] = { JMod.EZ_RESOURCE_TYPES.ALUMINUM, JMod.EZ_RESOURCE_TYPES.COPPER, JMod.EZ_RESOURCE_TYPES.STEEL },
+            //      ["rock"] = { JMod.EZ_RESOURCE_TYPES.CONCRETE, JMod.EZ_RESOURCE_TYPES.CERAMIC }
+            // }
+            //
+            //
+            // PrintTextInFrame( text )
+            //
+            string text = "Хоффман развил свою предыдущую работу по моделированию TFT [27] исследованиями на DC (постоянном токе) до модели тонкопленочного транзистора с длинным каналом, что очень важно при расчете зависимости подвижности от напряжения на затворе [46].";
+            // 
+            PrintTextInFrame(text);
+        }
+        static void PrintTextInFrame(string text)
+        {
+            // чтобы не писать кучу \n
+            string[] lines = WrapText(text, Console.WindowWidth - 6);
+            //
+            int maxLength = 0;
+            foreach (string line in lines)
+            {
+                // Капец Intellisense стал умный, строку как я и хотел предугадал
+                if (line.Length > maxLength) maxLength = line.Length;
+            }
+            // Стырил с какого-то сайта крутые символы ╔ ╗ ║ ╚ ╝ ═ (это не равно) даже не знал что такие в юникоде есть
+            // https://www.megacooltext.com/generator/big-letters/
+            Console.WriteLine("╔" + new string('═', maxLength + 2) + "╗");
+
+            foreach (string line in lines)
+            {
+                Console.WriteLine("║ " + line.PadRight(maxLength) + " ║");
+            }
+
+            Console.WriteLine("╚" + new string('═', maxLength + 2) + "╝");
+        }
+        static string[] WrapText(string text, int maxWidth)
+        {
+            string[] words = text.Split(' ');
+            System.Collections.Generic.List<string> lines = new System.Collections.Generic.List<string>();
+            string currentLine = "";
+
+            foreach (string word in words)
+            {
+                if ((currentLine.Length + word.Length + 1) <= maxWidth)
+                {
+                    currentLine += (currentLine.Length == 0) ? word : " " + word;
+                }
+                else
+                {
+                    lines.Add(currentLine);
+                    currentLine = word;
+                }
+            }
+            if (!string.IsNullOrEmpty(currentLine))
+                lines.Add(currentLine);
+            return lines.ToArray();
+        }
+    }
+}
